20ce636b88eecd0cfc9ce41c568f11d5f6bcfedc
ernscht <ernscht@gmail.com>
2019-04-03 08:07:41 +0200

    feat: use globby version 9.2

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const niv = require('npm-install-version');
 
 niv.install('globby@7.1.1');
 niv.install('globby@8.0.2');
-niv.install('globby@9.1.0');
+niv.install('globby@9.2.0');
 
 const globby_7 = niv.require('globby@7.1.1');
 const globby_8 = niv.require('globby@8.0.2');
-const globby_9 = niv.require('globby@9.1.0');
+const globby_9 = niv.require('globby@9.2.0');
 
 const globbySyncTest = require('./src/tasks/globby.sync');
 
 const testObjects = [
 	{ globby: globby_7, version: '7.1.1' },
 	{ globby: globby_8, version: '8.0.2' },
-	{ globby: globby_9, version: '9.1.0' },
+	{ globby: globby_9, version: '9.2.0' },
 ];
 
 // create cpu profile (for chrome devtools)
