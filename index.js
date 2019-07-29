f05a3227c05f0ce3d733b254d308e48e35345472
ernscht <ernscht@gmail.com>
2019-07-29 15:50:45 +0200

    chore: use globby 10.0.1

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const niv = require('npm-install-version');
 niv.install('globby@7.1.1');
 niv.install('globby@8.0.2');
 niv.install('globby@9.2.0');
-niv.install('globby@10.0.0');
+niv.install('globby@10.0.1');
 
 const globby_7 = niv.require('globby@7.1.1');
 const globby_8 = niv.require('globby@8.0.2');
 const globby_9 = niv.require('globby@9.2.0');
-const globby_10 = niv.require('globby@10.0.0');
+const globby_10 = niv.require('globby@10.0.1');
 
 const globbySyncTest = require('./src/tasks/globby.sync');
 
@@ -18,7 +18,7 @@ const testObjects = [
 	{ globby: globby_7, version: '7.1.1' },
 	{ globby: globby_8, version: '8.0.2' },
 	{ globby: globby_9, version: '9.2.0' },
-	{ globby: globby_10, version: '10.0.0' },
+	{ globby: globby_10, version: '10.0.1' },
 ];
 
 // create cpu profile (for chrome devtools)
