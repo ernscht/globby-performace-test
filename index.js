4505dd8162e34fcc26a7fc21f85a8784ffa77286
ernscht <ernscht@gmail.com>
2020-02-07 18:15:47 +0100

    feat: add globby v11

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ niv.install('globby@7.1.1');
 niv.install('globby@8.0.2');
 niv.install('globby@9.2.0');
 niv.install('globby@10.0.1');
+niv.install('globby@11.0.0');
 
 const globby_7 = niv.require('globby@7.1.1');
 const globby_8 = niv.require('globby@8.0.2');
 const globby_9 = niv.require('globby@9.2.0');
 const globby_10 = niv.require('globby@10.0.1');
+const globby_11 = niv.require('globby@11.0.0');
 
 const globbySyncTest = require('./src/tasks/globby.sync');
 
@@ -19,6 +21,7 @@ const testObjects = [
 	{ globby: globby_8, version: '8.0.2' },
 	{ globby: globby_9, version: '9.2.0' },
 	{ globby: globby_10, version: '10.0.1' },
+	{ globby: globby_11, version: '11.0.0' },
 ];
 
 // create cpu profile (for chrome devtools)
@@ -33,4 +36,6 @@ sleep(500);
 globbySyncTest(testObjects[2]);
 sleep(500);
 globbySyncTest(testObjects[3]);
+sleep(500);
+globbySyncTest(testObjects[4]);
 console.log('·-~-·-~-·-~·-~-·-~-·-~·-~-·-~-·-~·-~-·-~-·-~·-~-·-~-·-~·-~-·-~-·');
