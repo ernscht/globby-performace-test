ce01a4f6a6979472d6a39fb186545e586ef538c6
ernscht <ernscht@gmail.com>
2022-01-27 17:14:29 +0100

    chore: update globby versions

--- README.md
+++ README.md
@@ -11,13 +11,13 @@ npm start
 
 ## time needed:
 
-100 runs - node 12.18.0 - Windows 10
+100 runs - node 12.18.2 - Windows 10
 
-* globby 7.1.1: between 5 and 17 ms (average: 6ms)
-* globby 8.0.2: between 56 and 89 ms (average: 63ms)
-* globby 9.2.0: between 55 and 85 ms (average: 62ms)
-* globby 10.0.1: between 5 and 17 ms (average: 6ms)
-* globby 11.0.4: between 5 and 17 ms (average: 7ms)
+* globby 7.1.1: between 5 and 20 ms (average: 8ms)
+* globby 8.0.2: between 77 and 157 ms (average: 88ms)
+* globby 9.2.0: between 59 and 120 ms (average: 87ms)
+* globby 10.0.2: between 6 and 22 ms (average: 9ms)
+* globby 11.1.0: between 6 and 15 ms (average: 8ms)
 
 ## last but not least
 
--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const niv = require('npm-install-version');
 niv.install('globby@7.1.1');
 niv.install('globby@8.0.2');
 niv.install('globby@9.2.0');
-niv.install('globby@10.0.1');
-niv.install('globby@11.0.4');
+niv.install('globby@10.0.2');
+niv.install('globby@11.1.0');
 
 const testObjects = [
 	{ globby: niv.require('globby@7.1.1'), version: '7.1.1' },
 	{ globby: niv.require('globby@8.0.2'), version: '8.0.2' },
 	{ globby: niv.require('globby@9.2.0'), version: '9.2.0' },
-	{ globby: niv.require('globby@10.0.1'), version: '10.0.1' },
-	{ globby: niv.require('globby@11.0.4'), version: '11.0.4' },
+	{ globby: niv.require('globby@10.0.2'), version: '10.0.2' },
+	{ globby: niv.require('globby@11.1.0'), version: '11.1.0' },
 ];
 
 const globbySync = require('./src/tasks/globby.sync');
